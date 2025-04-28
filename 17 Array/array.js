f514ce683e5d48eb5ae8bc24df0e00b2abacb3eb
NguyenChuong10 <NguyenChuong22062002@gmail.com>
2025-04-28 18:40:44 +0700

    array

---
+++ 17 Array/array.js
@@ -0,0 +1,117 @@
+// todo kiểm tra các kiểu dữ liệu sau đó xuất ra bằng code thuần trong mảng
+
+
+
+// const aRR = ['chuong' , 1 ,2, 3 , true , false,]
+
+// console.log(aRR);
+
+// function testArray(n){
+
+//     for(let i = 0 ; i <= n.length ; i ++){
+//         if(typeof n[i] === `string`){
+//             console.log (n[i])
+//         }else if( typeof n[i] === `number`){
+//             console.log("kiểu dữ liệu số là " +n[i])
+//         }else if(typeof n[i] === `boolean`){
+//             console.log("kiểu dữ liêu true false là : " +n[i])  
+//         }else {
+//             console.log("đây không phải mảng .")
+//         }
+//     }
+//     return 
+// }
+
+// let result = testArray(aRR);
+
+
+// todo truy xuất phần từ của mảng 
+
+// let arr5 = [1 ,2 , 3, 4,5]
+// console.log (arr5[0]);
+
+
+// todo hàm concat nối mảng hiện tại với mảng khác  trả về mảng mới
+
+// let num  = [1, 2 , 3 ,4 ,5]
+
+// let nameList = [ "chương","đức"," vân ","bảo","hiếu" ]
+
+// let result = num.concat(nameList);
+// console.log(result)
+
+// todo hàm push() : thêm một hoặc nhiều phần tử vào cuối mảng gốc 
+
+//let arr =[1 ,2 ,3,4,5]
+
+//let result = arr.push(6);
+//console.log(result)
+
+//console.log(arr);
+
+// todo hàm unshilf : thêm một hoặc nhiều phần tử vào đầu mảng gốc.
+
+// let arr = [ 0 , 1 , 2 , 3 , 4 , 5 ,6]
+// let result   = arr.unshift(1);
+// console.log(result)
+// console.log(arr)
+
+// todo hàm pop() : xoá phần tử cuối trong mảng.
+
+// let arr = [ 0 , 1 ,2 ,3 ,4 ,5,]
+
+// console.log(arr.pop(0))
+
+// console.log(arr)
+
+// todo hàm shift() : xoá phâng tử đầu trong mảng .
+
+ //let arr = [0 ,1 ,2 ,3 ,4 ,5];
+
+ //arr.unshift(0);
+
+ //console.log(arr);
+
+ //todo hàm slice : cắt một mảng từ vị trí bắt đầu đến vị trí muốn kêt thức
+
+//   let arr = [ 1, 2 , 3 ,4 ,5]
+
+//  let sliceArray = arr.slice(1 , 4)
+
+// console.log(sliceArray);
+
+// todo hàm indexof : tìm giá trị lần đầu tiên xuất hiện trong mảng sau đó in ra vị trí index còn nếu không có trả về -1
+
+//  let arr = [ 1 , 2 ,3,4,2,5]
+
+//  console.log(arr.indexOf(2));
+//  console.log(arr.lastIndexOf(2));
+
+// todo reverse hàm đảo ngược chuỗi 
+
+// let arr = [0,1,2,3,4,5,6]
+
+// arr.reverse();
+
+// console.log(arr)
+
+// todo hàm join() nối các phần tử trong mảng thành chuỗi
+
+// let arr = [1  , "Chương" , 3,  4 ]
+
+// let result = arr.join(" - ")
+
+// console.log( result); 
+
+//todo Hàm splice : hàm này được sử dụng để xoá một vị trí trong mảng splice()
+
+// let arr = [ 2 , 3, 4 ,5 ,6, 7,6]
+
+
+// arr.splice(-2 , 1)
+
+
+// console.log(arr)
+
+
+
